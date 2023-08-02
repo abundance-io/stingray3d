72bf68756ec57c26022c674cfa97fefe3ce173ca
abundance <abundanceanyanwu@gmail.com>
2023-08-02 05:51:47 +0100

    setup meson

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <SDL2/SDL.h>
+
+int main()
+{
+    SDL_Window *win = SDL_CreateWindow(
+        "stingray3d",
+        SDL_WINDOWPOS_CENTERED,
+        SDL_WINDOWPOS_CENTERED,
+        100, 100, 0);
+
+    while (true)
+    {
+    }
+
+    return 0;
+}
